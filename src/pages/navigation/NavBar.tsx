86d191e2eddeb63059c6eda617ecc31727a40e83
KIPOKA <ladykipoka@gmail.com>
2026-06-30 12:16:21 +0200

    First commit

---
+++ src/pages/navigation/NavBar.tsx
@@ -0,0 +1,16 @@
+
+
+function NavBar() {
+  return (
+    <nav className='navbar'>
+      
+      <div className='nav-links text-red-500 space-between text-lg text-center justify-center'>
+        <a href='#'>Home</a>
+        <a href='#'>About</a>
+        <a href='#'>Contact</a>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,12 @@
+// src/navigation/RootNavigator.tsx
+
+
+import NavBar from '../pages/navigation/NavBar';
+ 
+
+export default function RootNavigator() { 
+
+  return (
+    <NavBar/>
+  );
+}
